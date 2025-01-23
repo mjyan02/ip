15a63399d607b0f27550b35ada7c2cd94c7746ed
mjyan02 <mjyan02@gmail.com>
2025-01-23 22:48:18 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,30 +7,72 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm \n" + logo + "\nWhat can I do for you?");
+        System.out.println("Hello! I'm Duke\n" + logo + "\nWhat can I do for you?");
 
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100]; // Array to store tasks
         int taskCount = 0;
 
         while (true) {
             String input = sc.nextLine();
-            String inputLowerCase = input.toLowerCase();
+            String[] inputArr = input.split(" ", 2); // Split arguments for marking and unmarking
+            String inputLowerCase = inputArr[0].toLowerCase();
 
-            if (inputLowerCase.equals("bye")) {
-                System.out.println("Bye! Hope you come back soon!!");
-                break;
-            } else if (inputLowerCase.equals("list")) {
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
-                }
-            } else {
-                tasks[taskCount] = input;
-                taskCount++;
-                System.out.println("added: " + input);
+            switch (inputLowerCase) {
+                case "bye":
+                    System.out.println("Bye! Hope you come back soon!!");
+                    sc.close();
+                    return;
+
+                case "list":
+                    listTasks(tasks, taskCount);
+                    break;
+
+                case "mark":
+
+                case "unmark":
+                    modifyTask(tasks, inputArr, inputLowerCase.equals("mark"), taskCount);
+                    break;
+
+                default:
+                    addTask(tasks, input, taskCount++);
             }
         }
+    }
 
-        sc.close();
+    // Method for listing tasks
+    private static void listTasks(Task[] tasks, int taskCount) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+    }
+
+    // Method for marking or unmarking tasks
+    private static void modifyTask(Task[] tasks, String[] inputArr, boolean isMarked, int taskCount) {
+        try {
+            int taskNumber = Integer.parseInt(inputArr[1]) - 1;
+
+            if (taskNumber >= 0 && taskNumber < taskCount) {
+                if (isMarked) {
+                    tasks[taskNumber].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                } else {
+                    tasks[taskNumber].markAsNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                }
+                System.out.println("  " + tasks[taskNumber]);
+            } else {
+                System.out.println("Invalid task number!");
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input format! Please specify a valid task number.");
+        }
+    }
+
+    // Method for adding new tasks
+    private static void addTask(Task[] tasks, String input, int taskCount) {
+        tasks[taskCount] = new Task(input);
+        System.out.println("added: " + input);
     }
 }
