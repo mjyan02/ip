8d93f22853ffb2b05775ea3fa7ea0c3a80016225
mjyan02 <mjyan02@gmail.com>
2025-02-02 12:30:34 +0800

    A-JavaDoc

--- src/main/java/Duke/Task/Event.java
+++ src/main/java/Duke/Task/Event.java
@@ -5,10 +5,28 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Represents an event task with a description, start date and end date.
+ */
 public class Event extends Task {
+    /**
+     * The start date of the event.
+     */
     protected LocalDate from;
+
+    /**
+     * The end date of the event.
+     */
     protected LocalDate to;
 
+    /**
+     * Constructs a new Event task with the given description, start date and end date.
+     *
+     * @param description The description of the event.
+     * @param from The start date of the event in yyyy-MM-dd format.
+     * @param to The end date of the event in yyyy-MM-dd format.
+     * @throws DukeException If the date format is invalid.
+     */
     public Event(String description, String from, String to) throws DukeException {
         super(description);
 
@@ -20,6 +38,15 @@ public class Event extends Task {
         }
     }
 
+    /**
+     * Constructs a new Event task with the given description, start date and end date.
+     *
+     * @param description The description of the event.
+     * @param from The start date of the event in yyyy-MM-dd format.
+     * @param to The end date of the event in yyyy-MM-dd format.
+     * @param isDone Specifies if task is marked
+     * @throws DukeException If the date format is invalid.
+     */
     public Event(String description, String from, String to, boolean isDone) throws DukeException {
         super(description, isDone);
 
@@ -31,11 +58,21 @@ public class Event extends Task {
         }
     }
 
+    /**
+     * Converts the task into a formatted string representation suitable for file storage on hard disk.
+     *
+     * @return A string representation of the task for file storage.
+     */
     @Override
     public String toFile() {
         return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
     }
 
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return The formatted string representation of the task.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) +
