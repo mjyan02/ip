2c42a4071559d94a199068b1b507ebefadd5c6c6
mjyan02 <mjyan02@gmail.com>
2025-02-04 15:35:53 +0800

    Duke.java: update switch statement indentation and fallthrough comments

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -38,62 +38,63 @@ public class Duke {
                 Keywords keyword = Parser.parseKeyword(fullKeyword);
 
                 switch (keyword) {
-                    case BYE:
-                        ui.displayMessage("Bye! Hope you come back soon!!");
-                        isExit = true;
-                        break;
+                case BYE:
+                    ui.displayMessage("Bye! Hope you come back soon!!");
+                    isExit = true;
+                    break;
 
-                    case LIST:
-                        tasks.listTasks(ui);
-                        break;
+                case LIST:
+                    tasks.listTasks(ui);
+                    break;
 
-                    case MARK:
+                case MARK:
+                // Fallthrough
 
-                    case UNMARK:
-                        String[] markArgs = fullKeyword.split(" ", 2);
-                        tasks.modifyTask(Integer.parseInt(markArgs[1]) - 1, keyword.equals(Keywords.MARK), ui, storage);
-                        break;
+                case UNMARK:
+                    String[] markArgs = fullKeyword.split(" ", 2);
+                    tasks.modifyTask(Integer.parseInt(markArgs[1]) - 1, keyword.equals(Keywords.MARK), ui, storage);
+                    break;
 
-                    case TODO:
-                        String[] todoArgs = fullKeyword.split(" ", 2);
-                        tasks.addTask(new Todo(todoArgs[1].trim()), ui, storage);
-                        break;
+                case TODO:
+                    String[] todoArgs = fullKeyword.split(" ", 2);
+                    tasks.addTask(new Todo(todoArgs[1].trim()), ui, storage);
+                    break;
 
-                    case DEADLINE:
-                        String deadlineDetails = fullKeyword.substring(fullKeyword.indexOf(" ") + 1);
-                        String[] deadlineArgs = deadlineDetails.split(" /by ", 2);
+                case DEADLINE:
+                    String deadlineDetails = fullKeyword.substring(fullKeyword.indexOf(" ") + 1);
+                    String[] deadlineArgs = deadlineDetails.split(" /by ", 2);
 
-                        if (deadlineArgs.length < 2) {
-                            throw new DukeException("Format: deadline <description> /by yyyy-MM-dd");
-                        }
+                    if (deadlineArgs.length < 2) {
+                        throw new DukeException("Format: deadline <description> /by yyyy-MM-dd");
+                    }
 
-                        tasks.addTask(new Deadline(deadlineArgs[0].trim(), deadlineArgs[1].trim()), ui, storage);
-                        break;
+                    tasks.addTask(new Deadline(deadlineArgs[0].trim(), deadlineArgs[1].trim()), ui, storage);
+                    break;
 
-                    case EVENT:
-                        String eventDetails = fullKeyword.substring(fullKeyword.indexOf(" ") + 1);
-                        String[] eventArgs = eventDetails.split(" /from ", 2);
+                case EVENT:
+                    String eventDetails = fullKeyword.substring(fullKeyword.indexOf(" ") + 1);
+                    String[] eventArgs = eventDetails.split(" /from ", 2);
 
-                        if (eventArgs.length < 2 || !eventArgs[1].contains(" /to ")) {
-                            throw new DukeException("Format: event <description> /from yyyy-MM-dd /to yyyy-MM-dd");
-                        }
+                    if (eventArgs.length < 2 || !eventArgs[1].contains(" /to ")) {
+                        throw new DukeException("Format: event <description> /from yyyy-MM-dd /to yyyy-MM-dd");
+                    }
 
-                        String[] eventTimes = eventArgs[1].split(" /to ", 2);
-                        tasks.addTask(new Event(eventArgs[0].trim(), eventTimes[0].trim(), eventTimes[1].trim()), ui, storage);
-                        break;
+                    String[] eventTimes = eventArgs[1].split(" /to ", 2);
+                    tasks.addTask(new Event(eventArgs[0].trim(), eventTimes[0].trim(), eventTimes[1].trim()), ui, storage);
+                    break;
 
-                    case DELETE:
-                        String[] deleteArgs = fullKeyword.split(" ", 2);
-                        tasks.deleteTask(Integer.parseInt(deleteArgs[1]) - 1, ui, storage);
-                        break;
+                case DELETE:
+                    String[] deleteArgs = fullKeyword.split(" ", 2);
+                    tasks.deleteTask(Integer.parseInt(deleteArgs[1]) - 1, ui, storage);
+                    break;
 
-                    case FIND:
-                        String[] findArgs = fullKeyword.split(" ", 2);
-                        tasks.findTasks(findArgs.length > 1 ? findArgs[1] : "", ui);
-                        break;
+                case FIND:
+                    String[] findArgs = fullKeyword.split(" ", 2);
+                    tasks.findTasks(findArgs.length > 1 ? findArgs[1] : "", ui);
+                    break;
 
-                    case UNKNOWN:
-                        throw new DukeException("I'm sorry, but I don't recognize that command :(");
+                case UNKNOWN:
+                    throw new DukeException("I'm sorry, but I don't recognize that command :(");
                 }
             } catch (NumberFormatException e) {
                 ui.displayError("Please enter a number!");
